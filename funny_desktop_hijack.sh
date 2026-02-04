0a19db65044a1ec82c947e50b0ea8c5f2cf015e5
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2026-02-04 14:02:00 -0600

    Implement fake ransomware warning and shutdown sequence
    Added a fake ransomware warning simulation with countdown and shutdown.

--- funny_desktop_hijack.sh
+++ funny_desktop_hijack.sh
@@ -21,4 +21,45 @@ else
   cmatrix &
 fi
 
-echo "[*] Done."
+#########################################
+# FAKE RANSOMWARE WARNING
+#########################################
+
+sleep 3
+clear
+
+tput setaf 1
+tput bold
+
+echo "████████████████████████████████████████████████████"
+echo "██                                                ██"
+echo "██            ⚠ SYSTEM COMPROMISED ⚠             ██"
+echo "██                                                ██"
+echo "████████████████████████████████████████████████████"
+echo
+echo "Your files have been replaced with penguins."
+echo
+echo "To recover your system, deposit:"
+echo
+echo "                1 BTC"
+echo
+echo "Failure to comply will result in system shutdown."
+echo
+echo "(Classroom Malware Simulation)"
+echo
+echo "----------------------------------------------------"
+echo
+
+tput sgr0
+
+# Countdown
+for i in {30..1}; do
+    echo -ne "System shutting down in $i seconds...\r"
+    sleep 1
+done
+
+echo
+echo "[!] Initiating shutdown..."
+
+# Real shutdown
+sudo shutdown -h now
